53a00d8084a63f9fedac20949d366d877f844a1b
Brian <brian@di-rs.com>
2023-02-08 23:59:17 +0000

    add check for in CWA

--- src/main/java/com/ClanFightPerformanceTracker/ClanFightPerformanceTrackerPlugin.java
+++ src/main/java/com/ClanFightPerformanceTracker/ClanFightPerformanceTrackerPlugin.java
@@ -1,5 +1,6 @@
 package com.ClanFightPerformanceTracker;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.inject.Provides;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import net.runelite.client.events.PlayerLootReceived;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+import org.apache.commons.lang3.ArrayUtils;
 
 import javax.inject.Inject;
 import java.text.DecimalFormat;
@@ -75,6 +77,7 @@ public class ClanFightPerformanceTrackerPlugin extends Plugin {
 	@Getter(AccessLevel.PACKAGE)
 	private int successfulSnares = 0;
 	private final Map<Skill, Integer> previousSkillExpTable = new EnumMap<>(Skill.class);
+	private static final Set<Integer> CLAN_WARS_ARENAS = ImmutableSet.of(12621,12622,12623,13647,13646,13645,13644,13133,13134,13135,13643,13642,13641,13898);
 
 	@Provides
 	ClanFightPerformanceTrackerConfig provideConfig(ConfigManager configManager) {
@@ -125,9 +128,8 @@ public class ClanFightPerformanceTrackerPlugin extends Plugin {
 
 		// we're being hit, should we start tank timer?
 		if (actor == (Actor) client.getLocalPlayer()) {
-			//TODO consider CWA
-			if (!client.getWorldType().contains(WorldType.PVP) && client.getVarbitValue(Varbits.IN_WILDERNESS) == 0) {
-				return; // if you aren't in wilderness on a regular world don't count the hits
+			if (!client.getWorldType().contains(WorldType.PVP) && client.getVarbitValue(Varbits.IN_WILDERNESS) == 0 && !isAtCWA()) {
+				return; // if you aren't in wilderness on a regular world or CWA don't count the hits
 			}
 			if(client.getVarbitValue(Varbits.MULTICOMBAT_AREA) == 0){
 				return; // if you aren't in multi you aren't tanking i.e 1vs1 isn't tanking
@@ -274,6 +276,21 @@ public class ClanFightPerformanceTrackerPlugin extends Plugin {
 		if(worldType.contains(WorldType.PVP) || worldType.contains(WorldType.BOUNTY) || worldType.contains(WorldType.HIGH_RISK)
 		|| worldType.contains(WorldType.DEADMAN) || worldType.contains(WorldType.SEASONAL) || worldType.contains(WorldType.TOURNAMENT_WORLD)){
 			return true;
+		}
+		return false;
+	}
+
+
+	public boolean isAtCWA()
+	{
+		final int[] mapRegions = client.getMapRegions();
+
+		for (int region : CLAN_WARS_ARENAS)
+		{
+			if (ArrayUtils.contains(mapRegions, region))
+			{
+				return true;
+			}
 		}
 		return false;
 	}
